eb81ffc2c3b1230b6203bdba26da2169ca212ad4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-27 15:32:52 -0300

    Add eslint, docker and env file

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+  console.log('Server is running on http://localhost:3000')
+})
