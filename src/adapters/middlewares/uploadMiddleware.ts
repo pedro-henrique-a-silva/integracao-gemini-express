c58c5eee217c3f03f4fd75884a2f2925265b1db7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-28 14:56:50 -0300

    chore: Add uploadMiddleware for validating image, customer code, measure datetime, and measure type

---
+++ src/adapters/middlewares/uploadMiddleware.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
+import { checkIfImageBase64IsValid } from '../../domain/utils/utils';
+import InvalidDataException from '../exceptions/InvalidDataException';
+
+export const checkIfBase64ImageIsValid = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { image } = req.body;
+  const scheme = z.string().base64();
+
+  const result = scheme.safeParse(image);
+
+  if (!result.success) throw new InvalidDataException('Image is not valid');
+
+  await checkIfImageBase64IsValid(image);
+
+  next();
+};
+
+export const checkIfCustomerCodeIsValid = (req: Request, res: Response, next: NextFunction) => {
+  const { customer_code: customerCode } = req.body;
+
+  const schema = z.string().uuid();
+
+  const result = schema.safeParse(customerCode);
+
+  if (!result.success) throw new InvalidDataException('Customer code is not valid');
+
+  next();
+};
+
+export const checkIfMeasureDateTimeIsValid = (req: Request, res: Response, next: NextFunction) => {
+  const { measure_datetime: measureDatetime } = req.body;
+
+  const schema = z.string().datetime();
+
+  const result = schema.safeParse(measureDatetime);
+
+  if (!result.success) throw new InvalidDataException('Measure datetime is not valid');
+
+  next();
+};
+
+export const checkIfMeasureTypeIsValid = (req: Request, res: Response, next: NextFunction) => {
+  const { measure_type: measureType } = req.body;
+  const availableMeasureType = ['WATER', 'GAS'] as const;
+
+  const schema = z.enum(availableMeasureType);
+
+  const result = schema.safeParse(measureType);
+
+  if (!result.success) throw new InvalidDataException('Measure type is not valid');
+
+  next();
+};
