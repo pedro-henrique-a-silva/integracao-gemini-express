1cc5019a8e7bbd759ed0697236eb88a5b7a62c24
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-28 14:56:18 -0300

    chore: Add validation middlewares to measureRoutes file

--- src/adapters/routes/measureRoutes.ts
+++ src/adapters/routes/measureRoutes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import measureController from '../controller/measureController';
-import { checkIfBase64ImageIsValid } from '../middlewares/uploadMiddleware';
+import {
+  checkIfBase64ImageIsValid,
+  checkIfCustomerCodeIsValid,
+  checkIfMeasureDateTimeIsValid,
+  checkIfMeasureTypeIsValid,
+} from '../middlewares/uploadMiddleware';
 
 const measureRouter = express.Router();
 
-measureRouter.post('/', checkIfBase64ImageIsValid, measureController);
+measureRouter.post(
+  '/',
+  checkIfBase64ImageIsValid,
+  checkIfCustomerCodeIsValid,
+  checkIfMeasureDateTimeIsValid,
+  checkIfMeasureTypeIsValid,
+  measureController,
+);
 
 export default measureRouter;
