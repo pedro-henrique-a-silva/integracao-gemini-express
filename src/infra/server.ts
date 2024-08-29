8f2e6bd5da0a8c70dae7f49b086ec60cc455b669
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 18:02:23 -0300

    refactor: Remove console.log statement from server.ts

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -3,6 +3,5 @@ import app from './app';
 const port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
-  console.log(port);
   console.log(`Server is running on http://localhost:${port}`);
 });
