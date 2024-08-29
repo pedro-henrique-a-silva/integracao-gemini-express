4eb0af00bfa0d092bb959fe9e3126d5131a9a5cd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 13:12:58 -0300

    refactor: Update measureController to include listMeasurentsController

--- src/adapters/controller/measureController.ts
+++ src/adapters/controller/measureController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
-import { processMeasurementConfirmation, processMeasurementUpload } from '../../domain/measuresDomain';
+import { 
+  processMeasurementConfirmation, 
+  processMeasurementUpload,
+  processMeasurementList 
+} from '../../domain/measuresDomain';
 import statusCode from '../statusCode';
+import { MeasureType } from '../../application/interface/Measure';
 
 export const uploadMeasurementController = async (req: Request, res: Response) => {
   const {
@@ -21,6 +26,15 @@ export const confirmMeasurementController = async (req: Request, res: Response) 
   const { measure_uuid, confirmed_value } = req.body;
 
   const result = await processMeasurementConfirmation(measure_uuid, confirmed_value);
+
+  return res.status(statusCode[result.status]).json(result.data);
+}
+
+export const listMeasurentsController = async (req: Request, res: Response) => {
+  const { customerId } = req.params;
+  const { measureType } = req.query;
+
+  const result = await processMeasurementList(customerId, measureType as MeasureType);
 
   return res.status(statusCode[result.status]).json(result.data);
 }
