9f2720bdcd0ca24aed7a01f83fcf67eadea886f2
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 11:17:49 -0300

    feat: Update measureController to include confirmMeasurementController

--- src/adapters/controller/measureController.ts
+++ src/adapters/controller/measureController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { processMeasurementUpload } from '../../domain/measuresDomain';
 import statusCode from '../statusCode';
 
-const uploadController = async (req: Request, res: Response) => {
+export const uploadMeasurementController = async (req: Request, res: Response) => {
   const {
     image,
     customer_code: customerCode,
@@ -17,4 +17,8 @@ const uploadController = async (req: Request, res: Response) => {
   return res.status(statusCode[result.status]).json(result.data);
 };
 
-export default uploadController;
+export const confirmMeasurementController = async (req: Request, res: Response) => {
+  const { measure_uuid, confirmed_value } = req.body;
+  return res.status(200).send('Confirm measurement');
+}
+
