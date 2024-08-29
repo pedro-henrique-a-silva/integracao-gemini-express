9bbe6342543d5c3c0b745e55feea359c06afed9d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 17:57:40 -0300

    refactor: Add getImageController to handle GET /image/:id endpoint

---
+++ src/adapters/controller/imageController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import getImage from "../../domain/imagesDomain";
+
+const getImageController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const image = await getImage(id);
+
+  const imageBuffer = Buffer.from(image.imageBase64Format, 'base64');
+  
+  res.set('Content-Type', image.mimeType);
+  res.send(imageBuffer);
+}
+
+export default getImageController;
