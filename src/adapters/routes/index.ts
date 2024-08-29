212deb49378039126e2c79115ab66eafaed5b023
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 18:00:07 -0300

    refactor: Add imageRoutes.ts and update rootRouter to handle image-related routes

--- src/adapters/routes/index.ts
+++ src/adapters/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import measureRouter from './measureRoutes';
+import imageRouter from './imageRoutes';
 
 const rootRouter = express.Router();
 
 rootRouter.use(measureRouter);
+rootRouter.use(imageRouter);
 
 export default rootRouter;
