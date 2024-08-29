a993c6193d38690919f8c3e8886a10cf9cb89709
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 18:02:16 -0300

    refactor: Update error handling in app.ts

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors';
+
 import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import routes from '../adapters/routes';
@@ -14,15 +15,16 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World');
 });
 
-app.use('/uploads', express.static(path.join(__dirname, '..', '..', 'tmp', 'uploads')));
-
 app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({
       errorCode: err.errorCode, errorDescription: err.message,
     });
   }
-  return res.status(500).json({ errorCode: 'internal_server_error', message: "Opss! Ocorreu um erro" });
+  return res.status(500).json({ 
+    errorCode: 'internal_server_error', 
+    message:  "Opss! Ocorreu um erro"
+  });
 });
 
 export default app;
