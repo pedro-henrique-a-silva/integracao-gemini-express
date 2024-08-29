5d992804e729153c1851045e8b2d9f51fa6cc3aa
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-08-29 00:04:48 -0300

    feat: Update Measure interface and related types

--- src/application/interface/Measure.ts
+++ src/application/interface/Measure.ts
@@ -1,15 +1,30 @@
-export interface MeasurementRequestPayload {
-  image: string,
-  customerCode: string
-  measureDatetime: string,
-  measureType: 'WATER' | 'GAS',
-}
+export type MeasureType = 'WATER' | 'GAS';
 
-export interface saveImageBase64IntoFileReturn {
+export type MeasurementRequestPayload = {
+  image: string,
+  custumerCode: string
+  measureDatetime: string,
+  measureType: MeasureType,
+
+};
+
+export type SavedImageInfo = {
   imagePath: string,
   mimeType: string,
-}
+};
 
-export interface MeasurementDataToPersist extends MeasurementRequestPayload {
+export type MeasureDataFromDB = {
+  id: string,
   measureValue: number,
-}
+  imageUrl: string,
+  custumerId: string
+  measureDate: Date,
+  confirmedValue: number | null,
+  measureType: MeasureType,
+  createdAt: Date | null,
+  updatedAt: Date | null,
+};
+
+export type MeasurementDataToPersist = Omit<
+MeasureDataFromDB, 'createdAt' | 'updatedAt' | 'confirmedValue' | 'id'
+>;
